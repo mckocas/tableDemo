ee359bd5a4160a96f525337159513a53bca083cd
mckocas <kocasmehmetcan@gmail.com>
2019-01-02 22:27:25 +0300

    Updates

---
+++ precache-manifest.b20cfab12e1539cba4abbd6c94c0b790.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/tableDemo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "91e1edcd02e845db9db9",
+    "url": "/tableDemo/static/css/main.80f6b5ca.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/tableDemo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "95b5a97b37d6d3a5748d",
+    "url": "/tableDemo/static/js/1.95b5a97b.chunk.js"
+  },
+  {
+    "revision": "0dc7a2d298b397d04384",
+    "url": "/tableDemo/static/js/runtime~main.0dc7a2d2.js"
+  },
+  {
+    "revision": "91e1edcd02e845db9db9",
+    "url": "/tableDemo/static/js/main.91e1edcd.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/tableDemo/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/tableDemo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/tableDemo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "95b5a97b37d6d3a5748d",
+    "url": "/tableDemo/static/css/1.1b8c6815.chunk.css"
+  },
+  {
+    "revision": "139c4f1afe73f0396312e1f7a3e41927",
+    "url": "/tableDemo/index.html"
+  }
+];
